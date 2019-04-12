213bff8ccfac7f8deaf635771aa4afe8e8507483
11081 <11081@N11081-03.lghitachi.co.kr>
2019-04-12 15:35:43 +0900

    1. 최초 업로드

---
+++ JUITest/src/main/java/com/jui/test/JuiTestApplication.java
@@ -0,0 +1,13 @@
+package com.jui.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuiTestApplication.class, args);
+	}
+
+}
---
+++ JUITest/src/test/java/com/jui/test/JuiTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jui.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JuiTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
